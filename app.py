c1619b59e65fedabdf3fa574f07363e92cc88163
Mohit-0918 <mohit.sharma.2001rip@outlook.com>
2023-06-08 18:34:28 +0530

    added get_task method, update_task method and delete_task method

--- app.py
+++ app.py
@@ -33,6 +33,46 @@ def create_task():
     db.session.commit()
     return jsonify({'message': 'Task created successfully'})
 
+#Method to get task with ID
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        task_data = {
+            'id': task.id,
+            'title': task.title,
+            'description': task.description,
+            'due_date': task.due_date,
+            'status': task.status
+        }
+        return jsonify(task_data)
+    return jsonify({'message': 'Task not found'}), 404
+
+#method to update the task
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        data = request.get_json()
+        if not data or 'title' not in data or 'due_date' not in data or 'status' not in data:
+            abort(400, 'Missing required fields')
+        task.title = data['title']
+        task.description = data.get('description', '')
+        task.due_date = data['due_date']
+        task.status = data['status']
+        db.session.commit()
+        return jsonify({'message': 'Task updated successfully'})
+    return jsonify({'message': 'Task not found'}), 404
+
+#method to delete the task
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify({'message': 'Task deleted successfully'})
+    return jsonify({'message': 'Task not found'}), 404
 
 
 
